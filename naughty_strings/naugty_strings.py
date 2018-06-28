6837251e3fc93ad975954978d2695be7b5b16bf1
Chad Estioco <chadestioco@gmail.com>
2018-06-28 18:43:46 +0800

    Actually provide naughty strings.

--- naughty_strings/naugty_strings.py
+++ naughty_strings/naugty_strings.py
@@ -1,5 +1,15 @@
-import json
+from faker.providers import BaseProvider
 
+import json
+import random
+
+NAUGHTY_LIST = []
 with open("blns.json") as naughties:
-    naughty_list = json.load(naughties)
-    print(naughty_list)
+    NAUGHTY_LIST = json.load(naughties)
+
+fake = Faker()
+
+class NaughtyProvider(BaseProvider):
+
+    def naughty_string(self):
+        return random.choice(NAUGHTY_LIST)
--- readme.markdown
+++ readme.markdown
@@ -3,6 +3,9 @@
 [Data fakers](https://github.com/joke2k/faker) that pull from [the big list of
 naughty strings](https://github.com/minimaxir/big-list-of-naughty-strings).
 
+Designed for edge-case testing. Do not expect data that will look sensible or
+presentable.
+
 # License
 
 MIT
