96c35384fc95d7c4091c57a921887f62d27dcaa8
Chad Estioco <chadestioco@gmail.com>
2018-07-02 21:58:04 +0800

    Next time, I'm really using testing.pypi.org.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="naughty fakers",
-    version="0.1.0",
+    name="naughties",
+    version="0.1.2",
     author="Chad Estioco",
     author_email="chadestioco@gmail.com",
-    url="https://github.com/skytreader/naughty-fakers",
-    packages=["naughty_fakers"],
-    data_files=[("", "blns.json")],
+    packages=["naughties"],
+    data_files=[("", ["blns.json"])],
     install_requires=["Faker==0.8.16"],
     license="MIT",
     description="Fakers that return naughty strings."
